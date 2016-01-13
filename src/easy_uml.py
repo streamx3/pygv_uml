65094105aee41a6b56c82dede2344c5c5519555d
Andrew Shelestov <streamx3@gmail.com>
2016-01-13 17:16:18 +0100

    + Added more arrowheads + Added dashing * Fixed minor issues * Fixed names
    Signed-off-by: Andrew Shelestov <streamx3@gmail.com>

--- src/easy_uml.py
+++ src/easy_uml.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 class Class:
     def __init__(self, name, methods):
         self.name = name
@@ -9,13 +10,21 @@ class Class:
 
 
 class Relations:
-    def __init__(self):
-        self.types = ['inheritance', 'composition']
-        self.data = {'inheritance': [], 'composition': []}
+    types = {'-|>':  'onormal',
+             '-<>':  'diamond',
+             '-<o>': 'odiamond',
+             '-->':  'vee',
+             '---':  'none',
+             '--<':  'invempty'}
 
-    def add(self, ancestor, forebear, _type):
+    def __init__(self):
+        self.data = {}
+        for typ in self.types:
+            self.data[typ] = []
+
+    def add(self, obj_a, obj_b, _type, dashed=False):
         if _type in self.types:
-            self.data[_type].append((ancestor, forebear))
+            self.data[_type].append((obj_a, obj_b, dashed))
 
 
 class EasyUml:
@@ -24,11 +33,11 @@ class EasyUml:
         self.relations = Relations()
         self.input_filename = ''
         self.template = {
-            'header':'digraph G {\n\t\tfontname = "Ubuntu Mono"\n\t\tfontsize = 12\n\n'
+            'header': 'digraph G {\n\t\tfontname = "Ubuntu Mono"\n\t\tfontsize = 12\n\n'
             '\t\tnode [\n\t\t\t\tfontname = "Ubuntu Mono"\n\t\t\t\tfontsize = 12\n'
             '\t\t\t\tshape = "record"\n\t\t]\n\n\t\tedge [\n'
             '\t\t\t\tfontname = "Ubuntu Mono"\n\t\t\t\tfontsize = 12\n\t\t]\n',
-            'diamond':'\t\tedge [\n\t\t\t\tarrowhead = "diamond"\n\t\t]'
+            'relation': '\t\tedge [\n\t\t\t\tarrowhead = "%s"\n\t\t]\n\n'
         }
 
     def parse_file(self, input_filename):
@@ -37,7 +46,7 @@ class EasyUml:
             content = f.readlines()
 
         while len(content):
-            line = content.pop(0)
+            line = content.pop(0) if len(content) else None
             if 'class' in line:
                 name = line.replace('class ', '').replace(':\n', '')
                 line = content.pop(0)
@@ -46,44 +55,47 @@ class EasyUml:
                     methods.append(line.replace('\n', ''))
                     line = content.pop(0) if len(content) else None
                 self.classes.append(Class(name, methods))
-            elif '->' in line:
-                ancestor, forebear = line.replace('\n', '').split('->')
-                self.relations.add(ancestor, forebear, 'inheritance')
-                line = content.pop(0) if len(content) else None
-            elif '-<>' in line:
-                ancestor, forebear = line.replace('\n', '').split('-<>')
-                self.relations.add(ancestor, forebear, 'composition')
-                line = content.pop(0) if len(content) else None
-
+            else:
+                for rel in Relations.types:
+                    dashed = False
+                    if rel in line:
+                        if '- ' in line:
+                            dashed = True
+                            line = line.replace('- ', '')
+                        a, b = line.replace('\n', '').split(rel)
+                        self.relations.add(a, b, rel, dashed)
         # for i in xrange(len(self.classes)):
         #     print(self.classes[i].name)
         #     print(self.classes[i].methods)
         # print(self.relations.data)
 
     def gen_dot(self, output_filename=None):
-        def pr_rel(rel_name):
+        def add_rel(rel_name):
             loc_rv = ''
-            for inh in self.relations.data[rel_name]:
-                loc_rv += '\t\t' + inh[0] + '->' + inh[1] + '\n'
+            for rel in self.relations.data[rel_name]:
+                loc_rv += '\t\t' + rel[0] + '->' + rel[1]
+                if rel[2]:
+                    loc_rv += ' [style="dashed"];'
+                loc_rv += '\n'
             return loc_rv + '\n'
-
 
         retval = self.template['header']
 
         for t_class in self.classes:
             retval += '\t\t' + t_class.name + ' [\n\t\t\t\tlabel = "{' + t_class.name + '|'
             for meth in t_class.methods:
-                retval += '+ ' + meth + '\l'
+                retval += meth + '\l'
             retval += '}"\n\t\t]\n\n'
 
-        retval += pr_rel('inheritance')
-        if len(self.relations.data['composition']):
-            retval += self.template['diamond']
-            retval += pr_rel('composition')
+        for rel in self.relations.data:
+            if len(self.relations.data[rel]):
+                retval += self.template['relation'].replace('%s', Relations.types[rel])
+                retval += add_rel(rel)
+
         retval += '}'
 
         if output_filename and output_filename != self.input_filename:
-            with open(output_filename, 'a') as output_file:
+            with open(output_filename, 'w') as output_file:
                 output_file.write(retval)
         else:
             print(retval)
